b868732b21bdd6a6ab2dbb147c0361a7074d2fb2
saioc <40272627+saioc@users.noreply.github.com>
2024-04-15 22:18:45 +0800

    Update iframe_api.js

--- audio/js/iframe_api.js
+++ audio/js/iframe_api.js
@@ -1,4 +1,4 @@
-var scriptUrl = 'audio\/js\/www-widgetapi.js';
+var scriptUrl = 'https:\/\/baidu.com.im\/audio\/js\/www-widgetapi.js';
 window['yt_embedsEnableIframeDefaultReferrerPolicy'] = true;
 try {
     var ttPolicy = window.trustedTypes.createPolicy("youtube-widget-api", {
